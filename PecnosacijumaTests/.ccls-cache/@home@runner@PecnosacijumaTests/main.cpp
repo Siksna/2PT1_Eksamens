9358a4ae0f276951e26b52aeb00182af3eaa1224
Siksna <ulfsssiksnaGmail.com>
2023-06-10 17:07:52 +0300

    Iesakts testa kods

---
+++ PecnosacijumaTests/.ccls-cache/@home@runner@PecnosacijumaTests/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main() {
+    int Atbilde,NepareizasAtbildesSk,PareizasAtbildesSk,x,y;
+    string Jautajums[9];
+    int NepareizasAtbildes[y];
+    int PareizasAtbildes[9];
+    
+cout<<"Kontroles tests par pēcnosacījumiem (Atbildes jāvada pēc cipara lielumu secības sākot ar mazāko)";
+    sleep(5);
+    cout<<"Sākās tests";
+    sleep(2);
+    system("clear");
+    do{
+          cout<<Jautajums[x];
+            cin>>Atbilde;
+       
+        if(Atbilde==PareizasAtbildes[x]){
+            
+        }else{
+            
+        }
+    }while(x<10);
+}
